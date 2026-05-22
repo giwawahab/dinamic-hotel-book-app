70e852146043402065f889984f105dbfe1c6eed2
giwawahab <giwawahabtomiwa2022@gmail.com>
2026-05-22 17:07:22 +0100

    load cabins data from supa using tanstack

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,3 +1,4 @@
+import { formatCurrency } from "../../utils/helpers";
 import styled from "styled-components";
 
 const TableRow = styled.div`
@@ -38,3 +39,20 @@ const Discount = styled.div`
   font-weight: 500;
   color: var(--color-green-700);
 `;
+
+function CabinRow({ cabin }) {
+  const { name, maxCapacity, regularPrice, discount, image } = cabin;
+
+  return (
+    <TableRow role="row">
+      <Img src={image} />
+      <Cabin>{name}</Cabin>
+      <div>Fits up to {maxCapacity} guests</div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Discount>{formatCurrency(discount)}</Discount>
+      <button>Delete</button>
+    </TableRow>
+  );
+}
+
+export default CabinRow;
