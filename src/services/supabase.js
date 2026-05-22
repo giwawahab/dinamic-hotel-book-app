9036e9d0a90130214404bc8428ec18bc81a294f8
giwawahab <giwawahabtomiwa2022@gmail.com>
2026-05-22 15:49:23 +0100

    setup and use superbase, setup and use tanstack

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://knrzyxqqvqycftkhfhvd.supabase.co";
+const supabaseKey = "sb_publishable_5wN3DbphzP479xhDcm0ybw_D8QDMaXx";
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+export default supabase;
