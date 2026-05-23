82eb040e9e5fc9939bf5738c178a5061d08326c6
giwawahab <giwawahabtomiwa2022@gmail.com>
2026-05-23 15:33:10 +0100

    Add and setup react hook form library

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,19 +1,26 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
+import Button from "../ui/Button";
+import { useState } from "react";
 
 function Cabins() {
-
+  const [showForm, setShowForm] = useState(false);
   return (
     <>
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>Filter / Sort</p>
-    </Row>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
 
-    <Row>
-      <CabinTable />
-    </Row>
+      <Row>
+        <CabinTable />
+        <Button onClick={() => setShowForm((showForm) => !showForm)}>
+          Add new cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
+      </Row>
     </>
   );
 }
