7d9dd19464db98cfea5069d9b38a5eb9c140f234
giwawahab <giwawahabtomiwa2022@gmail.com>
2026-06-11 20:23:38 +0100

    use the modal window to delete confirmation

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -2,41 +2,19 @@ import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
 import Modal from "../../ui/Modal";
 
-
-function AddCabin(){
-  return <Modal>
-    <Modal.Open opens="cabin-form">
-      <Button>Add new cabin</Button>
-    </Modal.Open>
-    <Modal.Window name="cabin-form">
-      <CreateCabinForm />
-    </Modal.Window>
-
-
-    {/* <Modal.Open opens="table">
-      <Button>Add new cabin</Button>
-    </Modal.Open>
-    <Modal.Window name="table">
-      <CreateCabinForm />
-    </Modal.Window> */}
-  </Modal>
+function AddCabin() {
+  return (
+    <div>
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
+      </Modal>
+    </div>
+  );
 }
-
-
-// function AddCabin() {
-//   const [isOpenModal, setIsOpenModal] = useState(false);
-//   return (
-//     <div>
-//       <Button onClick={() => setIsOpenModal((isOpenModal) => !isOpenModal)}>
-//         Add new cabin
-//       </Button>
-//       {isOpenModal && (
-//         <Modal onClose={() => setIsOpenModal(false)}>
-//           <CreateCabinForm onCloseModal={() => setIsOpenModal(false)}/>
-//         </Modal>
-//       )}
-//     </div>
-//   );
-// }
 
 export default AddCabin;
