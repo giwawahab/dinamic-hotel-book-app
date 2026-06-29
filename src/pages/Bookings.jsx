0a24271376c37fff57c41dc10bf914b0feaa9437
giwawahab <giwawahabtomiwa2022@gmail.com>
2026-06-29 16:20:28 +0100

    implement server side filtering

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,16 +1,17 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import BookingTable from "../features/bookings/BookingTable"
+import BookingTable from "../features/bookings/BookingTable";
+import BookingTableOperations from "../features/bookings/BookingTableOperations";
 
 function Bookings() {
   return (
     <>
-    <Row type="horizontal">
-      <Heading as="h1">All bookings</Heading>
-      <p>TEST</p>
-    </Row>
-    <BookingTable />
-  </>
+      <Row type="horizontal">
+        <Heading as="h1">All bookings</Heading>
+        <BookingTableOperations />
+      </Row>
+      <BookingTable />
+    </>
   );
 }
 
